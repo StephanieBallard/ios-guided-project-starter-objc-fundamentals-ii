407f56220a8f21db8c7d89419bffd977d4e933bd
Paul Solt <PaulSolt@gmail.com>
2020-05-12 12:55:20 -0400

    Added tip controller interface (to be implemented)

--- Tips/LSITipController.h
+++ Tips/LSITipController.h
@@ -8,9 +8,16 @@
 
 #import <Foundation/Foundation.h>
 
+// Build projects faster, and we can reference other classes "recursively"
+@class LSITip; // forward class declaration (IOU) here's a heads up about a new class
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITipController : NSObject
+
+@property NSArray<LSITip *> *tips;
+
+- (void)addTip:(LSITip *)tips;
 
 @end
 
--- Tips/LSITipController.m
+++ Tips/LSITipController.m
@@ -6,7 +6,9 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
+// Import statements copy/paste code into this file
 #import "LSITipController.h"
+#import "LSITip.h"      // #3 Import header files in your .m files
 
 @implementation LSITipController
 
