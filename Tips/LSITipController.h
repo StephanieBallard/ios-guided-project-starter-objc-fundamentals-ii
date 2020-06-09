ff831a7965db979457bcaaeb3c2cf321d31791af
Paul Solt <PaulSolt@gmail.com>
2020-06-09 13:38:10 -0400

    Added Tip Controller Logic

--- Tips/LSITipController.h
+++ Tips/LSITipController.h
@@ -8,9 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+// Forward class declaration (improve build times, refer between two files, prevents compilation cycle)
+@class LSITip;
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITipController : NSObject
+
+// lightweight generics make the NSArray have only LSITip objects
+
+// property attributes (* = default)
+//  nonatomic, atomic*
+//  readwrite*, readonly
+
+// tips is an Array of LSITips
+// Swift: [Tip]
+@property (nonatomic, readonly) NSArray<LSITip *> *tips;
+
+- (void)addTip:(LSITip *)tip;
 
 @end
 
--- Tips/LSITipController.m
+++ Tips/LSITipController.m
@@ -8,6 +8,43 @@
 
 #import "LSITipController.h"
 
+// Class Extension (private properties / method declarations)
+
+@interface LSITipController ()
+
+// readwrite by default
+@property (nonatomic) NSMutableArray *internalTips;
+
+@end
+
 @implementation LSITipController
+
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        // all properties/instance variables default to nil or 0, or 0.0
+        // Objc will clear the memory when you allocate
+        _internalTips = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+// Make our getter a computed property to return a readonly, immutable
+// array of tips
+- (NSArray<LSITip *> *)tips
+{
+    // var -> let
+    // copy on write (cow)
+    return [self.internalTips copy]; // NSMutableArray -> NSArray
+}
+
+- (void)addTip:(LSITip *)tip {
+    
+    // Rule 2: Always use self.propertyName to set/get outside of your init/dealloc/setters/getters
+    
+    [self.internalTips addObject:tip];
+}
 
 @end
