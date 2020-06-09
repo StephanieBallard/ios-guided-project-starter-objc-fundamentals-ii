07379fa58f7e6f981be945c8b9e163ac5b22af5c
Paul Solt <PaulSolt@gmail.com>
2020-06-09 12:58:50 -0400

    Added initializer and properties.

--- Tips/LSITip.h
+++ Tips/LSITip.h
@@ -12,6 +12,27 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LSITip : NSObject
 
+// FUTURE: tipAmount - double -> calculateTip (computed property?)
+
+// @property will create 3 things
+// 1. instance variable
+//NSString *_name;
+// 2. getter method
+//- (NSString *)name;
+// 3. setter method (readonly will disable a setter)
+//- (void)setName:(NSString *)name;
+
+@property NSString *name;
+@property double total;
+@property double tipPercentage;
+@property int splitCount;
+
+// Swift
+//init(withName name: String, total: Double, tipPercentage: Double, splitCount: Int)
+
+- (instancetype)initWithName:(NSString *)name total:(double)total tipPercentage:(double)tipPercentage splitCount:(int)splitCount;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- Tips/LSITip.m
+++ Tips/LSITip.m
@@ -10,4 +10,24 @@
 
 @implementation LSITip
 
+- (instancetype)initWithName:(NSString *)name
+                       total:(double)total
+               tipPercentage:(double)tipPercentage
+                  splitCount:(int)splitCount
+{
+    if (self = [super init]) {  // Be careful not to do a == it's a bug!
+        _name = name;
+        // total = total; // BUG: self assignment, objc will not complain!
+        _total = total;
+        _tipPercentage = tipPercentage;
+        _splitCount = splitCount;
+        
+        // RULE: Always use _propertyName for assignments in init/delloc/setter/getter
+        // This helps prevent side effects like syncing a half initalized
+        // object to firebase
+        
+    }
+    return self;
+}
+
 @end
